a34216c6b7abbf0961292adff6a4c30c3fd19c3d
Harrison Ku <31172836+hmku@users.noreply.github.com>
2018-07-10 00:38:58 -0700

    Added search bar and display search results

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template, request
+import parser
+app = Flask(__name__)
+
+@app.route('/')
+def main():
+    return render_template("index.html")
+
+@app.route('/search-results',methods=['POST'])
+def get_search_results():
+    search = request.form['searchKey']
+    return parser.search_lyrics(search)
+
+if __name__ == "__main__":
+    app.run()
+    
---
+++ parser.py
@@ -0,0 +1,13 @@
+import requests
+
+GOOGLE_API_KEY = 'AIzaSyCrBdeuM-QHwP-tRxlF8Il9XB9WgBcnOro'
+BASE_URL = 'https://www.googleapis.com/customsearch/v1?key={key}&cx={cx}&q={query}'
+CX = '004866776918846137660:g-xyfvdi1va'
+
+def search_lyrics(input):
+    r = requests.get(BASE_URL.format(key=GOOGLE_API_KEY, cx=CX, query=input))
+    results = r.json()
+    ret = ''
+    for item in results['items']:
+        ret += item['title'] + ', '
+    return ret

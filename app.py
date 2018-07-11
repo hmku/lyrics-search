95d4d0650e60f24a61121c24fe7a25a5d2f00851
hmku <harrisonmku@gmail.com>
2018-07-10 21:30:51 -0700

    Fix unclear file name

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request
-import parser
+import engine
 app = Flask(__name__)
 
 @app.route('/')
@@ -9,7 +9,7 @@ def main():
 @app.route('/search-results',methods=['POST'])
 def get_search_results():
     search = request.form['searchKey']
-    return parser.search_lyrics(search)
+    return engine.search_lyrics(search)
 
 if __name__ == "__main__":
     app.run()

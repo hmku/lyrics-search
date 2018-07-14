142e6c5e108fab7c7d13236ced8e2837f2f8c2cd
Vincent Xu <vincentxu9@gmail.com>
2018-07-13 23:59:29 -0700

    Moved code into a single youtube link function that returns the most popular link given search query

--- youtube_link_scraper.py
+++ youtube_link_scraper.py
@@ -2,11 +2,13 @@ import urllib.request
 import urllib.parse
 import re
 
-query_input = "Alec Benjamin Let Me Down Slowly"
-query_string = urllib.parse.urlencode({"search_query" : query_input})
-html_content = urllib.request.urlopen("http://www.youtube.com/results?" + query_string)
-search_results = re.findall(r'href=\"\/watch\?v=(.{11})', html_content.read().decode())
-print("http://www.youtube.com/watch?v=" + search_results[0])
+def youtube_link(query_input):
+    query_string = urllib.parse.urlencode({"search_query" : query_input})
+    html_content = urllib.request.urlopen("http://www.youtube.com/results?" + query_string)
+    search_results = re.findall(r'href=\"\/watch\?v=(.{11})', html_content.read().decode())
+    return ("http://www.youtube.com/watch?v=" + search_results[0])
+
+print(youtube_link("Childish Gambino This is America"))
 
 
 # https://www.codeproject.com/Articles/873060/Python-Search-Youtube-for-Video

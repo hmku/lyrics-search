3417246da960e0a342dc86f305c6608273ad4b7c
hmku <harrisonmku@gmail.com>
2018-07-14 00:40:03 -0700

    Change module/function names

---
+++ google_scraper.py
@@ -0,0 +1,10 @@
+import urllib.request
+import urllib.parse
+import re
+
+
+def get_youtube_link(query_input):
+    query_string = urllib.parse.urlencode({"search_query" : query_input})
+    html_content = urllib.request.urlopen("http://www.youtube.com/results?" + query_string)
+    search_results = re.findall(r'href=\"\/watch\?v=(.{11})', html_content.read().decode())
+    return ("http://www.youtube.com/watch?v=" + search_results[0])
--- youtube_scraper.py
+++ youtube_scraper.py
@@ -3,12 +3,12 @@ import urllib.parse
 import re
 
 
-def get_youtube_link(query_input):
-    query_string = urllib.parse.urlencode({"search_query" : query_input})
+def get_top_youtube_link(input):
+    query_string = urllib.parse.urlencode({"search_query" : input})
     html_content = urllib.request.urlopen("http://www.youtube.com/results?" + query_string)
     search_results = re.findall(r'href=\"\/watch\?v=(.{11})', html_content.read().decode())
     return ("http://www.youtube.com/watch?v=" + search_results[0])
 
 
-#If the video URL is https://www.youtube.com/watch?v=xxxxxxxxxxxx
-#The thumbnail URL is https://i.ytimg.com/vi/xxxxxxxxxxxx/maxresdefault.jpg
+# If the video URL is https://www.youtube.com/watch?v=xxxxxxxxxxxx
+# The thumbnail URL is https://i.ytimg.com/vi/xxxxxxxxxxxx/maxresdefault.jpg

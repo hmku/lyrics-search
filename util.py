ecb85ba26fce3d964fe15a05350fbb2b4294a909
hmku <harrisonmku@gmail.com>
2018-07-14 00:09:50 -0700

    Add parsing for snippets

--- util.py
+++ util.py
@@ -1,11 +1,3 @@
 def split_name_str(title):
     name = title.split(' Lyrics - ')
     return dict(zip(['artist', 'title'], name))
-
-
-
-# Grab link from json
-# Video link from google?
-# Snippets from google?
-# Image
-

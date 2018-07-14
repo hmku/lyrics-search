5571948267de9dc7b04f00ae032fdca41512f329
hmku <harrisonmku@gmail.com>
2018-07-14 00:42:54 -0700

    Add youtube link to song object in search_lyrics

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request
 import engine
 import util
-import youtube_link_scraper
+import youtube_scraper
 
 
 app = Flask(__name__)
@@ -17,21 +17,19 @@ def search_lyrics():
     query = request.form['query']
     results = engine.get_search_results(query)
     songs = []
-    links = []
+
     for song in results['items']:
         song_name = util.split_name_str(song['title'])
+        youtube_query = song_name['artist'] + ' ' + song_name['title']
+        youtube_link = youtube_scraper.get_top_youtube_link(youtube_query)
         d = {
             'title': song_name['title'],
             'artist': song_name['artist'], 
             'link': song['link'],
+            'youtube': youtube_link,
         }
         songs.append(d)
-        youtube_query = song_name['artist'] + ' ' + song_name['title']
-        print(youtube_query)
-        links.append(youtube_link_scraper.get_youtube_link(youtube_query))
-        print(youtube_link_scraper.get_youtube_link(youtube_query))
     
-    print(links)
     return 'Top song: ' + songs[0]['title'] # TODO: Create template to return
 
 

979bc2ca92cf17ff640e89af67bc9736d1855780
hmku <harrisonmku@gmail.com>
2018-07-14 15:22:24 -0700

    Rename and fix google_search_result.py to google_scraper.py

--- google_scraper.py
+++ google_scraper.py
@@ -1,10 +1,59 @@
-import urllib.request
-import urllib.parse
-import re
+import requests
+import time
+from bs4 import BeautifulSoup
+ 
+ 
+USER_AGENT = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36'}
+ 
+ 
+def _search_results(query, number_results, language):
+    assert isinstance(query, str), 'Search term must be a string'
+    assert isinstance(number_results, int), 'Number of results must be an integer'
+    replaced_search_term = query.replace(' ', '+')
+ 
+    google_url = 'https://www.google.com/search?q={}&num={}&hl={}&as_sitesearch=AZlyrics.com'.format(replaced_search_term, number_results, language)
+    response = requests.get(google_url, headers=USER_AGENT)
+    response.raise_for_status()
+ 
+    return query, response.text
 
 
-def get_search_results(input): # TODO: Actually get results from Google
-    query = urllib.parse.urlencode({"search_query" : input})
-    page = urllib.request.urlopen("http://www.youtube.com/results?" + query)
-    search_results = re.findall(r'href=\"\/watch\?v=(.{11})', page.read().decode())
-    return ("http://www.youtube.com/watch?v=" + search_results[0])
+def _parse_results(html, keyword):
+    soup = BeautifulSoup(html, 'html.parser')
+ 
+    found_results = []
+    result_block = soup.find_all('div', attrs={'class': 'g'})
+    for result in result_block:
+        link = result.find('a', href=True)
+        title = result.find('h3', attrs={'class': 'r'})
+        if link and title:
+            link = link['href']
+            title = title.get_text()
+            if link != '#':
+                found_results.append({'title': title, 'link': link})
+    return found_results
+
+
+def _scrape_google(search_term, number_results, language_code):
+    try:
+        keyword, html = _search_results(search_term, number_results, language_code)
+        results = _parse_results(html, keyword)
+        return results
+    except AssertionError:
+        raise Exception("Incorrect arguments parsed to function")
+    except requests.HTTPError:
+        raise Exception("You appear to have been blocked by Google")
+    except requests.RequestException:
+        raise Exception("Appears to be an issue with your connection")
+
+
+def search_list(keywords, number_of_results):
+    data = []
+    try:
+        results = _scrape_google(keywords, number_of_results, "en")
+        for result in results:
+            data.append(result)
+    except Exception as e:
+        return e
+    
+    return data
